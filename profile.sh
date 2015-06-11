ca9be32f160463ecfda75ec621b0c799931d47b0
Iain Wood <iain@soulflyer.co.uk>
2015-06-11 09:19:36 +0700

    tweak

--- profile.sh
+++ profile.sh
@@ -88,7 +88,7 @@ NewLine="\n"
 Jobs="\j"
 
 
-# This PS1 snippet was adopted from code for MAC/BSD I saw from: http://allancraig.net/index.php?option=com_content&view=article&id=108:ps1-export-command-for-git&catid=45:general&Itemid=96
+# This PS1 snippet was adopted from code for MAC/BSD http://allancraig.net/index.php?option=com_content&view=article&id=108:ps1-export-command-for-git&catid=45:general&Itemid=96
 
 export PS1='$PS1START$(git branch &>/dev/null;\
 if [ $? -eq 0 ]; then \
