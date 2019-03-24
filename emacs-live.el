1bcae44fd4cf88f58cd2fdaf4360a68ad0fe64fe
iain <iain@soulflyer.co.uk>
2019-03-24 13:10:18 +0700

    added emacs-live config file

---
+++ emacs-live.el
@@ -0,0 +1,17 @@
+;;; package --- Summary
+;;; Code:
+;;; Commentary:
+
+(live-use-packs '(stable/package-pack
+                  stable/foundation-pack
+                  stable/colour-pack
+                  stable/lang-pack
+                  stable/power-pack
+                  stable/git-pack
+                  stable/org-pack
+                  stable/clojure-pack
+                  stable/bindings-pack
+                  ~/Code/emacs-live-pack))
+
+;; (live-add-packs '(~/Code/emacs-live-pack))
+;;; .emacs-live.el ends here
