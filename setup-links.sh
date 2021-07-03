7d91d59b800f108c3919e7a1ff7ebb1051a63607
iain <iain@soulflyer.co.uk>
2021-07-03 18:13:45 +0700

    Added stuff for acceptance

--- setup-links.sh
+++ setup-links.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 INSTALL_DIR=~/Code/profile/
+HOST=.`hostname | tr '[:upper:]' '[:lower:]' | sed s/\.local// | sed s/\.site//`
 
 ln -s $INSTALL_DIR/profile      ~/.profile
 ln -s $INSTALL_DIR/bashrc       ~/.bashrc
 ln -s $INSTALL_DIR/bash_profile ~/.bash_profile
+
+ln -s $INSTALL_DIR/${HOST}_rc   ~/.${HOST}_rc
+ln -s $INSTALL_DIR/${HOST}_profile  ~/.${HOST}_profile
