585226ffb660bb76f8098ddd965fb736beee675b
iain <iain@soulflyer.co.uk>
2025-08-13 19:24:35 +0700

    Added comics script

---
+++ bin/comics.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for link in \
+    http://xkcd.com/ \
+    https://sinfest.dreamhosters.com/ \
+    http://www.gocomics.com/luann \
+    http://www.gocomics.com/calvinandhobbes \
+    http://www.jesusandmo.net/ \
+    http://www.smbc-comics.com/ \
+    http://www.leasticoulddo.com/
+do
+    open $link
+done
